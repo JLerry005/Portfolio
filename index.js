ea64fddf949d76483a201044815c802f28da7cbc
JLerry05 <jlerry005@gmail.com>
2022-07-20 09:26:11 +0800

    Latest Version

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
-const person = {
-    first: name
-};
 
-const name = "James";
+// This is my first Function
+
+// function number() {
+//     console.log(42)
+// }
+
+// number();
+
+// =============================================
+
+// Create a function that logs out the sum of all lap times
+
+// let lap1 = 34;
+// let lap2 = 33;
+// let lap3 = 36;
+
+// function totalLapTimes(){
+//     let total = lap1 + lap2 + lap3;
+//     console.log(total);
+// }
+
+// totalLapTimes()
+
+// =============================================
+
+let lapsCompleted = 0
+
+// Create a function that increments the lapsCompletd variable with one
+// Run it three times
+
+function incrementOne() {
+
+    lapsCompleted = lapsCompleted + 1; 
+
+}
+
+incrementOne()
+incrementOne()
+incrementOne()
+
+console.log(lapsCompleted)
+
+
+
+
