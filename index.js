577d87e3ab213b32ce9733fa60694db6c088b280
JLerry05 <jlerry005@gmail.com>
2022-07-22 22:44:06 +0800

    Latest

--- index.js
+++ index.js
@@ -24,23 +24,52 @@
 
 // =============================================
 
-let lapsCompleted = 0
+// let lapsCompleted = 0
 
-// Create a function that increments the lapsCompletd variable with one
-// Run it three times
+// // Create a function that increments the lapsCompletd variable with one
+// // Run it three times
 
-function incrementOne() {
+// function incrementOne() {
 
-    lapsCompleted = lapsCompleted + 1; 
+//     lapsCompleted = lapsCompleted + 1; 
 
-}
+// }
 
-incrementOne()
-incrementOne()
-incrementOne()
+// incrementOne()
+// incrementOne()
+// incrementOne()
 
-console.log(lapsCompleted)
+// console.log(lapsCompleted)
 
+// =============================================
+// let countEl = document.getElementById("count-el");
+
+// let count = 0;
+
+// // Increment the people entered
+// function increment() {
+//     count = count + 1;
+//     countEl.innerText = count;
+// }
+
+// // Decrement the people Entered
+// function decrement() {
+//     count = count - 1; 
+//     countEl.innerText = count;
+// }
+
+// =============================================
+// STRING 
+let username = "ler"
+
+let message = "You have three new notification";
+// console.log(message + ", " + username + "!");
+
+let messageToUser = message + ", " + username + "!";
+console.log(messageToUser);
+
+// 
+// =============================================
 
 
 
